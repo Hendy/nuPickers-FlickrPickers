6b285ffd25b7f95baeed7f524862258d75ef2836
Hendy Racher <git@hendyracher.co.uk>
2014-07-23 22:11:46 +0100

    changed from a class library to a web project (for razor intellisense)

--- source/nuPickers-FlickrPickers/Properties/AssemblyInfo.cs
+++ source/nuPickers-FlickrPickers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nuPickers-FlickrPickers")]
+[assembly: AssemblyTitle("nuPickers_FlickrPickers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nuPickers-FlickrPickers")]
+[assembly: AssemblyProduct("nuPickers_FlickrPickers")]
 [assembly: AssemblyCopyright("Copyright ?  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c97ae081-a8e3-4ea0-beb0-2a065decdec8")]
+[assembly: Guid("a2fb3e61-67b5-4c66-82f8-c34e2d92fbbd")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.1.2.0")]
 [assembly: AssemblyVersion("0.1.2.0")]
 [assembly: AssemblyFileVersion("0.1.2.0")]
